a46e80e2e31fe139486edc3b4117e05233db90f4
İlayda Kaçkın <ilaydaozdemir0522@gmail.com>
2024-05-09 18:22:41 +0300

    Cari Ekle Sayfası yapıldı (Sql hatası veriyor) Cari listesi yapıldı.

--- WebApplication11/Controllers/CariController.cs
+++ WebApplication11/Controllers/CariController.cs
@@ -5,18 +5,123 @@ using System.Web.UI.WebControls;
 using WebApplication11.Models;
 using WebApplication11.Models.Dto;
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Web;
 
 namespace WebApplication11.Controllers
 {
     public class CariController : Controller
     {
-        // GET: Cari
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            IlListesiniOlustur();
+            return View();
+        }
+
+        [HttpPost]
         public ActionResult Ekle(CariDto cariDto)
         {
+            if (!ModelState.IsValid)
+            {
+                IlListesiniOlustur();
+                return View();
+            }
             SqlCommand cmd = new SqlCommand();
             cmd.CommandText = "p_CariKaydet";
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@ID  ","");
+            cmd.Parameters.AddWithValue("@ID", "");
+            cmd.Parameters.AddWithValue("@UyelikID", GetCookie("UyelikID"));
+            cmd.Parameters.AddWithValue("@Aktif", cariDto.Aktif);
+            cmd.Parameters.AddWithValue("@KayitTarihi  ", DateTime.Now);
+            cmd.Parameters.AddWithValue("@Kod", cariDto.Kod);
+            cmd.Parameters.AddWithValue("@Isim", cariDto.Isim);
+            cmd.Parameters.AddWithValue("@Unvan", cariDto.Unvan);
+            cmd.Parameters.AddWithValue("@Adres", cariDto.Adres);
+            cmd.Parameters.AddWithValue("@Ilce", cariDto.Ilce);
+            cmd.Parameters.AddWithValue("@Il", cariDto.Il);
+            cmd.Parameters.AddWithValue("@Ulke", cariDto.Ulke);
+            cmd.Parameters.AddWithValue("@Bolge ", cariDto.Bolge);
+            cmd.Parameters.AddWithValue("@TCKimlikNo", cariDto.TCKimlikNo);
+            cmd.Parameters.AddWithValue("@VergiDairesi", cariDto.VergiDairesi);
+            cmd.Parameters.AddWithValue("@VergiNumarasi", cariDto.VergiNumarasi);
+            cmd.Parameters.AddWithValue("@PostaKodu", cariDto.PostaKodu);
+            cmd.Parameters.AddWithValue("@Alici", cariDto.Alici);
+            cmd.Parameters.AddWithValue("@Satici", cariDto.Satici);
+            cmd.Parameters.AddWithValue("@Personel", cariDto.Personel);
+            cmd.Parameters.AddWithValue("@Telefon1", cariDto.Telefon1);
+            cmd.Parameters.AddWithValue("@Telefon2", cariDto.Telefon2);
+            cmd.Parameters.AddWithValue("@EMail", cariDto.EMail);
+            cmd.Parameters.AddWithValue("@Faks", cariDto.Faks);
+            cmd.Parameters.AddWithValue("@CepTelefonu", cariDto.CepTelefonu);
+            cmd.Parameters.AddWithValue("@WebSite", cariDto.WebSite);
+            cmd.Parameters.AddWithValue("@GrupKodu1ID", cariDto.GrupKodu1ID);
+            cmd.Parameters.AddWithValue("@GrupKodu2ID", cariDto.GrupKodu2ID);
+            cmd.Parameters.AddWithValue("@GrupKodu3ID", cariDto.GrupKodu3ID);
+            cmd.Parameters.AddWithValue("@GrupKodu4ID", cariDto.GrupKodu4ID);
+            cmd.Parameters.AddWithValue("@GrupKodu5ID", cariDto.GrupKodu5ID);
+            cmd.Parameters.AddWithValue("@GrupKodu6ID", cariDto.GrupKodu6ID);
+            cmd.Parameters.AddWithValue("@MuhasebeKodu", cariDto.MuhasebeKodu);
+            cmd.Parameters.AddWithValue("@Kilitli", cariDto.Kilitli);
+            cmd.Parameters.AddWithValue("@KilitAciklamasi", cariDto.KilitAciklamasi);
+            cmd.Parameters.AddWithValue("@DovizID", "");
+            cmd.Parameters.AddWithValue("@VadeGunu", cariDto.VadeGunu);
+            cmd.Parameters.AddWithValue("@Iskonto1", cariDto.Iskonto1);
+            cmd.Parameters.AddWithValue("@ListeFiyat", cariDto.ListeFiyat);
+            cmd.Parameters.AddWithValue("@Aciklama1", cariDto.Aciklama1 == null ? string.Empty : cariDto.Aciklama1);
+            cmd.Parameters.AddWithValue("@Aciklama2", cariDto.Aciklama2 == null ? string.Empty : cariDto.Aciklama2);
+            cmd.Parameters.AddWithValue("@Aciklama3", cariDto.Aciklama3 == null ? string.Empty : cariDto.Aciklama3);
+            cmd.Parameters.AddWithValue("@Aciklama4", cariDto.Aciklama4 == null ? string.Empty : cariDto.Aciklama4);
+            cmd.Parameters.AddWithValue("@Aciklama5", cariDto.Aciklama5 == null ? string.Empty : cariDto.Aciklama5);
+            cmd.Parameters.AddWithValue("@Aciklama6", cariDto.Aciklama6 == null ? string.Empty : cariDto.Aciklama6);
+            cmd.Parameters.AddWithValue("@LimitAsimindaUyar", cariDto.LimitAsimindaUyar);
+            cmd.Parameters.AddWithValue("@LimitAsimindaDurdur", cariDto.LimitAsimindaDurdur);
+            cmd.Parameters.AddWithValue("@CekSenetRiski", cariDto.CekSenetRiski);
+            cmd.Parameters.AddWithValue("@Limit", cariDto.Limit);
+            cmd.Parameters.AddWithValue("@ServisPersoneli", cariDto.ServisPersoneli);
+            cmd.Parameters.AddWithValue("@KullaniciAdi ", cariDto.KullaniciAdi);
+            cmd.Parameters.AddWithValue("@Parola", cariDto.Parola);
+            cmd.Parameters.AddWithValue("@RiskAciklama", cariDto.RiskAciklama);
+            cmd.Parameters.AddWithValue("@PlasiyerID", "");
+            cmd.Parameters.AddWithValue("@AnaCariID", "");
+            cmd.Parameters.AddWithValue("@TeslimCariID", "");
+            cmd.Parameters.AddWithValue("@KullaniciID", GetCookie("KullaniciID"));
+            DataTable dt = (DataTable)IDVeritabani.Sorgula(cmd, SorgulaTuru.Tablo);
+
+            return RedirectToAction("Liste");
+        }
+        [HttpGet]
+        public ActionResult Duzenle(string id)
+        {
+            var uyelikId = GetCookie("UyelikID");
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "p_Cari";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ID", id);
+            cmd.Parameters.AddWithValue("@UyelikID", uyelikId);
+
+            DataTable dt = (DataTable)IDVeritabani.Sorgula(cmd, SorgulaTuru.Tablo);
+
+            return View(dt);
+        }
+        [HttpPost]
+        public ActionResult Duzenle(CariDto cariDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                IlListesiniOlustur();
+                return View();
+            }
+
+            cariDto.UyelikID = GetCookie("UyelikID");
+
+            cariDto.ID = GetCookie("ID");
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "p_CariKaydet";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ID", GetCookie("UyelikID"));
+            cmd.Parameters.AddWithValue("@UyelikID", GetCookie("UyelikID"));
             cmd.Parameters.AddWithValue("@Aktif ", cariDto.Aktif);
             cmd.Parameters.AddWithValue("@KayitTarihi  ", DateTime.Now);
             cmd.Parameters.AddWithValue("@Kod", cariDto.Kod);
@@ -68,20 +173,77 @@ namespace WebApplication11.Controllers
             cmd.Parameters.AddWithValue("@Parola", cariDto.Parola);
             cmd.Parameters.AddWithValue("@RiskAciklama", cariDto.RiskAciklama);
             cmd.Parameters.AddWithValue("@PlasiyerID", "");
-            cmd.Parameters.AddWithValue("@AnaCariID  ","");
+            cmd.Parameters.AddWithValue("@AnaCariID  ", "");
             cmd.Parameters.AddWithValue("@TeslimCariID  ", "");
             cmd.Parameters.AddWithValue("@KullaniciID  ", cariDto.KullaniciID);
-           
+            DataTable dt = (DataTable)IDVeritabani.Sorgula(cmd, SorgulaTuru.Tablo);
 
-            return View();
-            
+            return RedirectToAction("Liste");
         }
-        public ActionResult Liste()
+        public ActionResult Liste(CariDto cariDto)
         {
-            return View();
+           
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "p_CariListesi";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@UyelikID", GetCookie("UyelikID"));
+            cmd.Parameters.AddWithValue("@Kod ", cariDto.Kod);
+            cmd.Parameters.AddWithValue("@Isim  ", cariDto.Isim);
+            cmd.Parameters.AddWithValue("@Unvan", cariDto.Unvan);
+            cmd.Parameters.AddWithValue("@TCKimlikNo", cariDto.TCKimlikNo);
+            cmd.Parameters.AddWithValue("@VergiNumarasi", cariDto.VergiNumarasi);
+            cmd.Parameters.AddWithValue("@CepTelefonu", cariDto.CepTelefonu);
+
+            DataTable dt = (DataTable)IDVeritabani.Sorgula(cmd, SorgulaTuru.Tablo);
+
+            return View(dt);
         }
+
+
+        #region Cookie İşlemleri
+        private string GetCookie(string name)
+        {
+            //Böyle bir cookie mevcut mu kontrol ediyoruz
+            if (Request.Cookies.AllKeys.Contains(name))
+            {
+                //böyle bir cookie varsa bize geri değeri döndürsün
+                return Server.UrlDecode(Request.Cookies[name].Value);
+            }
+            return null;
+        }
+
+        private class KullaniciListesi
+        {
+
+        }
+       
+
+        public void IlListesiniOlustur()
+        {
+            // Il Listesi oluşturma 
+            SqlCommand ilCommand = new SqlCommand();
+            ilCommand.CommandText = "p_GrupKoduListesi";
+            ilCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            ilCommand.Parameters.AddWithValue("@UyelikID", GetCookie("UyelikID"));
+
+            ilCommand.Parameters.AddWithValue("@Kod", "Il");
+            ilCommand.Parameters.AddWithValue("@AranacakKelime", "");
+
+
+            DataTable ilDataTable = (DataTable)IDVeritabani.Sorgula(ilCommand, SorgulaTuru.Tablo);
+
+            var iller = new List<string>();
+
+            for (int i = 0; i < ilDataTable.Rows.Count; i++)
+            {
+                iller.Add(Convert.ToString(ilDataTable.Rows[i]["Deger"]));
+            }
+            ViewBag.Iller = iller;
+        }
+
+        #endregion
     }
 
-    
+
 
 }
--- WebApplication11/Models/Dto/CariDto.cs
+++ WebApplication11/Models/Dto/CariDto.cs
@@ -11,28 +11,28 @@ namespace WebApplication11.Models.Dto
     {
         public string ID { get; set; }
         public string UyelikID { get; set; }
-        public string Aktif { get; set; }
+        public bool Aktif { get; set; } = false;
         public string KayitTarihi { get; set; }
-        public string Kod { get; set; }
-        public string Isim { get; set; }
-        public string Unvan { get; set; }
+        public string Kod { get; set; } = string.Empty;
+        public string Isim { get; set; } = string.Empty;
+        public string Unvan { get; set; } = string.Empty;
         public string Adres { get; set; }
         public string Ilce { get; set; }
         public string Il { get; set; }
         public string Ulke { get; set; }
         public string Bolge { get; set; }
-        public string TCKimlikNo { get; set; }
+        public string TCKimlikNo { get; set; } = string.Empty;
         public string VergiDairesi { get; set; }
-        public string VergiNumarasi { get; set; }
+        public string VergiNumarasi { get; set; } = string.Empty;
         public string PostaKodu { get; set; }
-        public string Alici { get; set; }
-        public string Satici { get; set; }
-        public string Personel { get; set; }
+        public bool Alici { get; set; } = false;
+        public bool Satici { get; set; } = false;
+        public bool Personel { get; set; } = false;
         public string Telefon1 { get; set; }
         public string Telefon2 { get; set; }
         public string EMail { get; set; }
         public string Faks { get; set; }
-        public string CepTelefonu { get; set; }
+        public string CepTelefonu { get; set; } = string.Empty;
         public string WebSite { get; set; }
         public string GrupKodu1ID { get; set; }
         public string GrupKodu2ID { get; set; }
@@ -41,12 +41,12 @@ namespace WebApplication11.Models.Dto
         public string GrupKodu5ID { get; set; }
         public string GrupKodu6ID { get; set; }
         public string MuhasebeKodu { get; set; }
-        public string Kilitli { get; set; }
+        public bool Kilitli { get; set; } = false;
         public string KilitAciklamasi { get; set; }
         public string DovizID { get; set; }
-        public string VadeGunu { get; set; }
-        public string Iskonto1 { get; set; }
-        public string ListeFiyat { get; set; }
+        public int VadeGunu { get; set; }
+        public decimal Iskonto1 { get; set; }
+        public int ListeFiyat { get; set; }
         public string Aciklama1 { get; set; }
         public string Aciklama2 { get; set; }
         public string Aciklama3 { get; set; }
@@ -54,11 +54,11 @@ namespace WebApplication11.Models.Dto
         public string Aciklama4 { get; set; }
         public string Aciklama5 { get; set; }
         public string Aciklama6 { get; set; }
-        public string LimitAsimindaUyar { get; set; }
-        public string LimitAsimindaDurdur { get; set; }
-        public string CekSenetRiski { get; set; }
-        public string Limit { get; set; }
-        public string ServisPersoneli { get; set; }
+        public bool LimitAsimindaUyar { get; set; }
+        public bool LimitAsimindaDurdur { get; set; } = false;
+        public bool CekSenetRiski { get; set; } = false;
+        public decimal Limit { get; set; }
+        public bool ServisPersoneli { get; set; } = false;
         public string KullaniciAdi { get; set; }
         public string Parola { get; set; }
         public string RiskAciklama { get; set; }
