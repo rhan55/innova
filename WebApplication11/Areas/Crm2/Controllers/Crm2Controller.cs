f74a7054a02b8294bbea2280416fc72f5f5d228f
Yunus KÖSE <yunuskose@outlook.com>
2024-08-13 18:46:09 +0300

    Crm

---
+++ WebApplication11/Areas/Crm2/Controllers/Crm2Controller.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using YKPortal.Areas.Satinalma.Controllers;
+using YKPortal.Models;
+using YKPortal.Models.YKClasses;
+
+namespace YKPortal.Areas.Crm2.Controllers
+{
+    public class Crm2Controller : Controller
+    {
+        public ActionResult AnaSayfa()
+        {
+            if (!AutoGirisKontrol())
+                return Redirect("~/YK/Giris");
+
+
+            return View();
+        }
+
+
+        #region Cookie İşlemleri
+
+
+        public bool AutoGirisKontrol()
+        {
+            bool GirisKontrol = false;
+
+            string KullaniciAdi = GetCookie("KullaniciAdi");
+            string Parola = GetCookie("Parola");
+
+            if (KullaniciAdi != null)
+            {
+
+                ViewBag.KullaniciAdi = KullaniciAdi;
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "p_KullaniciGirisi";
+                cmd.Parameters.AddWithValue("@KullaniciAdi", KullaniciAdi);
+                cmd.Parameters.AddWithValue("@Parola", Parola);
+                DataTable dt = (DataTable)IDVeritabani.Sorgula(cmd, SorgulaTuru.Tablo);
+
+
+                if (dt.Rows.Count > 0)
+                {
+                    GirisKontrol = true;
+                }
+                else
+                {
+                    GirisKontrol = false;
+                }
+            }
+
+            return GirisKontrol;
+        }
+
+        private void CreateCookie(string name, string value)
+        {
+            HttpCookie cookieVisitor = new HttpCookie(name, Server.UrlEncode(value));
+            // cookieVisitor.Expires = DateTime.Now.AddDays(2);
+            Response.Cookies.Add(cookieVisitor);
+        }
+        private string GetCookie(string name)
+        {
+            //Böyle bir cookie mevcut mu kontrol ediyoruz
+            if (Request.Cookies.AllKeys.Contains(name))
+            {
+                //böyle bir cookie varsa bize geri değeri döndürsün
+                return Server.UrlDecode(Request.Cookies[name].Value);
+            }
+            return null;
+        }
+        private void DeleteCookie(string name)
+        {
+            //Böyle bir cookie var mı kontrol ediyoruz
+            if (GetCookie(name) != null)
+            {
+                //Varsa cookiemizi temizliyoruz
+                Response.Cookies.Remove(name);
+                //ya da 
+                Response.Cookies[name].Expires = DateTime.Now.AddDays(-1);
+            }
+        }
+
+        #endregion
+    }
+
+}
---
+++ WebApplication11/Areas/Crm2/Crm2AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace YKPortal.Areas.Crm2
+{
+    public class Crm2AreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Crm2";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Crm2_default",
+                "Crm2/{controller}/{action}/{id}",
+                new { Contoller = "Crm2", action = "AnaSayfa", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
