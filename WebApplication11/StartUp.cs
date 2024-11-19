d1ae87d6dd2d3a0ca39e1da2cba9615689a6b1a6
İlayda Kaçkın <ilaydaozdemir0522@gmail.com>
2024-11-19 18:33:27 +0300

    MesajHub, StartUp ve chat sayfasında çalışıldı. Chat firefox anlık mesaj alamama sorunu düzeltildi

--- WebApplication11/StartUp.cs
+++ WebApplication11/StartUp.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
 using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Web;
 
 [assembly: OwinStartup(typeof(YKPortal.Startup))]
@@ -14,7 +16,13 @@ namespace YKPortal
     {
         public void Configuration(IAppBuilder app)
         {
-            // SignalR'ı başlatıyoruz
+            //ÖNEMLİ!eğer KeepAlive = 10 saniye ise, DisconnectTimeout en az 30 saniye olmalıdır.Eğer KeepAlive = 20 saniye ise, DisconnectTimeout en az 60 saniye olmalıdır.
+
+            GlobalHost.Configuration.DisconnectTimeout = TimeSpan.FromSeconds(60);
+   
+            GlobalHost.Configuration.KeepAlive = TimeSpan.FromSeconds(20);
+
+            // SignalR rotasını belirleyin
             app.MapSignalR();
          
           }
