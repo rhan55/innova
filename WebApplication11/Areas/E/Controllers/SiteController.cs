312455278b637cb2cc897eee1cf410f40adf0926
Yunus KÖSE <yunuskose@outlook.com>
2024-11-24 17:34:04 +0300

    E-Ticaret 1

---
+++ WebApplication11/Areas/E/Controllers/SiteController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using YKPortal.Areas.Satinalma.Controllers;
+using YKPortal.Models;
+using YKPortal.Models.YKClasses;
+
+namespace YKPortal.Areas.E.Controllers
+{
+    public class SiteController : Controller
+    {
+        public ActionResult AnaSayfa()
+        {
+
+
+            return View();
+        }
+
+        #region Cookie İşlemleri
+
+
+
+        private void CreateCookie(string name, string value)
+        {
+            HttpCookie cookieVisitor = new HttpCookie(name, Server.UrlEncode(value));
+            // cookieVisitor.Expires = DateTime.Now.AddDays(2);
+            Response.Cookies.Add(cookieVisitor);
+        }
+        private string GetCookie(string name)
+        {
+            //Böyle bir cookie mevcut mu kontrol ediyoruz
+            if (Request.Cookies.AllKeys.Contains(name))
+            {
+                //böyle bir cookie varsa bize geri değeri döndürsün
+                return Server.UrlDecode(Request.Cookies[name].Value);
+            }
+            return null;
+        }
+        private void DeleteCookie(string name)
+        {
+            //Böyle bir cookie var mı kontrol ediyoruz
+            if (GetCookie(name) != null)
+            {
+                //Varsa cookiemizi temizliyoruz
+                Response.Cookies.Remove(name);
+                //ya da 
+                Response.Cookies[name].Expires = DateTime.Now.AddDays(-1);
+            }
+        }
+
+        #endregion
+    }
+
+}
---
+++ WebApplication11/Areas/E/EAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace YKPortal.Areas.E
+{
+    public class IAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "E";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "E_default",
+                "E/{controller}/{action}/{id}",
+                new { Contoller = "Site", action = "AnaSayfa", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
