80dda3ee574f2ff7f1bf00c4dd2e62741890fce2
İlayda Kaçkın <ilaydaozdemir0522@gmail.com>
2024-12-13 18:33:38 +0300

    genel duzeltmeler ve tasarım iyileştirmesi yapıldı

--- WebApplication11/Areas/E/Controllers/ETicaretBilgilerController.cs
+++ WebApplication11/Areas/E/Controllers/ETicaretBilgilerController.cs
@@ -18,16 +18,6 @@ namespace YKPortal.Areas.E.Controllers
     {
         public ActionResult GenelBilgiler()
         {
-            var ETicaretTelefon = GrupKodListesiniGetir("ETicaretTelefon");
-            var ETicaretEmail = GrupKodListesiniGetir("ETicaretEmail");
-            var ETicaretLogo = GrupKodListesiniGetir("ETicaretLogo");
-            var ETicaretFirmaAdi = GrupKodListesiniGetir("ETicaretFirmaAdi");
-
-            ViewBag.ETicaretTelefon = ETicaretTelefon;
-            ViewBag.ETicaretEmail = ETicaretEmail;
-            ViewBag.ETicaretLogo = ETicaretLogo;
-            ViewBag.ETicaretFirmaAdi = ETicaretFirmaAdi;
-
             return View();
         }
         [HttpPost]
@@ -105,34 +95,6 @@ namespace YKPortal.Areas.E.Controllers
         }
 
 
-        private List<GrupKoduDto> GrupKodListesiniGetir(string kodAdi)
-        {
-            // GrupKodu1 Listesi oluşturma 
-            SqlCommand grupKodCommand = new SqlCommand();
-            grupKodCommand.CommandText = "p_GrupKoduListesi";
-            grupKodCommand.CommandType = System.Data.CommandType.StoredProcedure;
-
-            grupKodCommand.Parameters.AddWithValue("@Kod", kodAdi);
-            grupKodCommand.Parameters.AddWithValue("@UyelikID", UyelikIDGetir());
-
-            grupKodCommand.Parameters.AddWithValue("@AranacakKelime", "");
-
-
-            DataTable grupKoduTable = (DataTable)IDVeritabani.Sorgula(grupKodCommand, SorgulaTuru.Tablo);
-
-            List<GrupKoduDto> entities = new List<GrupKoduDto>();
-
-            for (int i = 0; i < grupKoduTable.Rows.Count; i++)
-            {
-                GrupKoduDto entity = new GrupKoduDto();
-                entity.ID = Convert.ToString(grupKoduTable.Rows[i]["ID"]);
-                entity.Kod = Convert.ToString(grupKoduTable.Rows[i]["Kod"]);
-                entity.Deger = Convert.ToString(grupKoduTable.Rows[i]["Deger"]);
-                entity.Aktif = Convert.ToBoolean(grupKoduTable.Rows[i]["Aktif"]);
-                entities.Add(entity);
-            }
-
-            return entities;
-        }
+        
     }
 }
