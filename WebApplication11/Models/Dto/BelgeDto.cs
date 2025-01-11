00bdb6ada86400ef0899ce4b2911feeb378cb1b7
İlayda Kaçkın <ilaydaozdemir0522@gmail.com>
2025-01-11 15:21:19 +0300

    EFatura işlemleri devam ediyor

--- WebApplication11/Models/Dto/BelgeDto.cs
+++ WebApplication11/Models/Dto/BelgeDto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using YKEFaturaEntegrasyon.EFaturaEDM;
 
 namespace YKPortal.Models.Dto
 {
@@ -43,8 +44,29 @@ namespace YKPortal.Models.Dto
         public string DepoGirisID { get { return _DepoGirisID ?? ""; } set { _DepoGirisID = value; } }
         public string DepoCikisID { get { return _DepoCikisID ?? ""; } set { _DepoCikisID = value; } }
         public List<BelgeKalemDto> Kalemler { get { return _Kalemler == null ? new List<BelgeKalemDto>() : _Kalemler; } set { _Kalemler = value; } }
-    }
 
+    }
+    public class InvoiceDto
+    {
+        public string Customer { get; set; } // Fatura alıcı ismi
+        public string Supplier { get; set; } // Tedarikçi
+        public decimal Amount { get; set; } // Fatura tutarı
+        public DateTime IssueDate { get; set; } // Fatura tarihi
+        public Guid ID { get; set; } // Fatura UUID
+        public int CariID { get; set; } // Cari ID
+        public string Unvan { get; set; } // Firma unvanı
+        public DateTime BaslangicTarihi { get; set; } // Fatura başlangıç tarihi
+        public INVOICEHEADER HEADER { get; set; }
+        public string UUID { get; set; }
+        public base64Binary CONTENT { get; set; }
+        public string SENDER { get; set; }
+        public string RECEIVER { get; set; }
+        public string SUPPLIER { get; set; }
+        public string CUSTOMER { get; set; }
+        public DateTime ISSUE_DATE { get; set; }
+        public bool ISSUE_DATESpecified { get; set; }
+        public byte[] Value { get; set; }
+    }
     public enum BelgeTipi
     {
         Genel,
