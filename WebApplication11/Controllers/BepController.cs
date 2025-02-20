ddb900ae8f8e936549e7e941658f8ca23e9ea2b2
Yunus KÖSE <yunuskose@outlook.com>
2025-02-20 11:29:07 +0300

    Bep Başlangıç

---
+++ WebApplication11/Controllers/BepController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YKPortal.Models;
+
+namespace YKPortal.Controllers
+{
+    public class BepController : Controller
+    {
+        // GET: Bep
+        public ActionResult Index()
+        {
+            if (!AutoGirisKontrol())
+                return Redirect("~/YK/Giris");
+
+            string UyelikID = GetCookie("UyelikID"));
+            string KullaniciID = GetCookie("KullaniciID"));
+
+
+
+
+            return View();
+        }
+
+
+
+
+
+
+        public bool AutoGirisKontrol()
+        {
+            bool GirisKontrol = false;
+
+            string KullaniciAdi = GetCookie("KullaniciAdi");
+            string Parola = GetCookie("Parola");
+
+            if (KullaniciAdi != null)
+            {
+
+                ViewBag.KullaniciAdi = KullaniciAdi;
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "p_KullaniciGirisi";
+                cmd.Parameters.AddWithValue("@KullaniciAdi", KullaniciAdi);
+                cmd.Parameters.AddWithValue("@Parola", Parola);
+                DataTable dt = (DataTable)IDVeritabani.Sorgula(cmd, SorgulaTuru.Tablo);
+
+                if (dt.Rows.Count > 0)
+                {
+                    string Bilgi = Convert.ToString(dt.Rows[0]["Bilgi"]);
+                    if (!Bilgi.StartsWith("UYARI!"))
+                    {
+                        DeleteCookie("UyelikBitisTarihi");
+                        CreateCookie("UyelikBitisTarihi", Convert.ToString(dt.Rows[0]["UyelikBitisTarihi"]));
+                        GirisKontrol = true;
+                    }
+                    else
+                    {
+                        GirisKontrol = false;
+                    }
+                }
+                else
+                {
+                    GirisKontrol = false;
+                }
+            }
+
+            return GirisKontrol;
+        }
+
+        private void CreateCookie(string name, string value)
+        {
+            HttpCookie cookieVisitor = new HttpCookie(name, Server.UrlEncode(value));
+            // cookieVisitor.Expires = DateTime.Now.AddDays(2);
+            Response.Cookies.Add(cookieVisitor);
+        }
+
+
+        private string GetCookie(string name)
+        {
+            //Böyle bir cookie mevcut mu kontrol ediyoruz
+            if (Request.Cookies.AllKeys.Contains(name))
+            {
+                //böyle bir cookie varsa bize geri değeri döndürsün
+                return Server.UrlDecode(Request.Cookies[name].Value);
+            }
+            return null;
+        }
+        private void DeleteCookie(string name)
+        {
+            //Böyle bir cookie var mı kontrol ediyoruz
+            if (GetCookie(name) != null)
+            {
+                //Varsa cookiemizi temizliyoruz
+                Response.Cookies.Remove(name);
+                //ya da 
+                Response.Cookies[name].Expires = DateTime.Now.AddDays(-1);
+            }
+        }
+    }
+}
